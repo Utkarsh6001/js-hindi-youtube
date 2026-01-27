d01d32540c09afb6d0794728881a03aab8d08cfa
Utkarsh6001 <utkarshkaushal6001@gmail.com>
2026-01-27 12:20:34 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code As newer version 
+
+//console.log(3+3) // we are using node.js, not browser
+
+//console.log(3+3) 
+let name = "utkarsh";
+let age = 22;
+let isLoggedIn = false;
+
+
+// number => 2 to power 53
+// bigint use for big number 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => variable not assigned
+// symbol => unique value
+
+ console.log(typeof "utkarsh");//string
+ console.log(typeof 6);//number
+ console.log(typeof null);//object
