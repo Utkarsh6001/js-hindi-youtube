cdcfdfa4582225280583e2b04c61cba14d612068
Utkarsh6001 <utkarshkaushal6001@gmail.com>
2026-01-27 13:04:19 +0000

    conversion of dataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+//output is nummber 
+let score1 = 33
+
+console.log(typeof score1);
+console.log(typeof (score1));
+
+//output is string 
+let score2 = "33"
+
+console.log(typeof score2);
+console.log(typeof (score2));
+
+//conversion operation
+let valueInNumber = Number(score2)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);//NaN gives
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+// null => 0
+// undefined => NaN
