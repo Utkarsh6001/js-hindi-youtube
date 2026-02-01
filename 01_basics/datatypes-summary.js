1f996531ffae6ac63f68f8afd1964fc9c8a210b6
Utkarsh6001 <utkarshkaushal6001@gmail.com>
2026-02-01 09:47:10 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,28 @@
+// (Primitive)
+// 7 types : String , Number , Boolean , Null , Undefined , Symbol , BigInt
+
+const score = 100; // Number    
+const scoreValue = 100.3; // String     
+const isLoggedIn = false; // Boolean
+const outsideTemp = null; // Null
+let userEmail; // Undefined
+const id = Symbol("123"); // Symbol
+const bigNumber = 3456789012345678901234567890n; // BigInt
+
+
+
+// (Non primitive) 
+ //Reference : Array , Object , Function 
+
+const heroes = ["Shaktiman" , "Naagraj" , "Doga"] // Array
+
+let MyObj = {  // Object
+    name : "Utkarsh",
+    age : 22,
+ };
+
+const myFunction = function(){  // Function
+    console.log("Hello World");
+ }
+
+ console.log(typeof scoreValue);
