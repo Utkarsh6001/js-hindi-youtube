43866cf1d3a41bb9234989367366230520f81e2f
Utkarsh6001 <utkarshkaushal6001@gmail.com>
2026-02-01 09:54:25 +0000

    change in location of file

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,3 +21,48 @@ console.log(valueInNumber);//NaN gives
 // true => 1 , false => 0
 // null => 0
 // undefined => NaN
+
+//*************************************Operation****************************************/
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "Hello"
+let str2 = "utkarsh"
+
+let str3 = str1 + str2 
+console.log(str3);
+
+//agar pehle string h toh string conversion hoga 
+//console.log("1" + 2);
+//  console.log(1 + "2");
+//  console,log("1" + 2 + 2);
+//  console.log(1 + 2 + "2");
+
+//console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+ 
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
